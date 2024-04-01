4daec11d43d31ed509ce711190f83a7dd038befa
Maicon Guedes <maicon.ag17@hotmail.com>
2024-04-01 20:43:15 -0300

    faculdade 1

--- Faculdade/Faculdade.sql
+++ Faculdade/Faculdade.sql
@@ -1,4 +1,4 @@
-CREATE DATEBASE Faculdade;
+CREATE database Faculdade;
 USE Faculdade;
 
 CREATE TABLE Depto  (
@@ -78,9 +78,49 @@ PRIMARY KEY (CodDepto);
 
 ALTER TABLE Disciplina
 ADD CONSTRAINT pk_Disciplina
-PRIMARY KEY (NumDisc);
+PRIMARY KEY (CodDepto, NumDisc);
+
+ALTER TABLE PreReq
+ADD CONSTRAINT pk_PreReq
+PRIMARY KEY (CodDeptoPreReq, NumDiscPreReq, CodDepto, NumDisc);
+
+ALTER TABLE Turma
+ADD CONSTRAINT pk_Turma
+PRIMARY KEY (AnoSem, CodDepto, NumDisc, SiglaTur);
+
+ALTER TABLE Horario
+ADD CONSTRAINT pk_Horario
+PRIMARY KEY (AnoSem, CodDepto, NumDisc, SiglaTur, DiaSem, HoraInicio);
+
+ALTER TABLE Predio
+ADD CONSTRAINT pk_Predio
+PRIMARY KEY (CodPred);
+
+ALTER TABLE Sala
+ADD CONSTRAINT pk_Sala
+PRIMARY KEY (CodPred, NumSala);
+
+ALTER TABLE Professor
+ADD CONSTRAINT pk_Professor
+PRIMARY KEY (CodProf);
+
+ALTER TABLE ProfTurma
+ADD CONSTRAINT pk_ProfTurma
+PRIMARY KEY (AnoSem, CodDepto, NumDisc, SiglaTur, CodProf);
+
+ALTER TABLE Titulacao
+ADD CONSTRAINT pk_Titulacao
+PRIMARY KEY (CodTit);
 
 ALTER TABLE Disciplina
 ADD CONSTRAINT fk_Discipli_Relation_Depto
 FOREIGN KEY (CodDepto) REFERENCES CodDepto(Depto);
+
+ALTER TABLE PreReq
+ADD CONSTRAINT fk_PreReq_Tem_Pre_Discipli
+FOREIGN KEY (CodDepto, NumDisc) REFERENCES CodDepto, NumDisc(Disciplina);
+
+ALTER TABLE PreReq
+ADD CONSTRAINT fk_PreReq_Eh_Pre_Discipli
+FOREIGN KEY (CodDeptoPreReq, NumDiscPreReq) REFERENCES CodDepto, NumDisc(Disciplina);
 
